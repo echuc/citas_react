1610a4b0588d0cc097825e3eaafbc749b538e355
echuc <qroomexico@yahoo.com.mx>
2024-02-13 00:12:17 -0500

    Se hizo el cambio en el header
    Commit realizado con éxito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 function Header( ) {
     return (
-            <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto text-indigo-900">
+            <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto text-indigo-900">
                 Seguimiento Pacientes {' '}
-                <span className="text-indigo-600">Veterinaria</span>
+                <span className="text-indigo-600">Veterinaria</span>{' '}
+                <span className="text-orange-600 text-2xl">by EsdrasCC</span>
             </h1>
     )
 }
